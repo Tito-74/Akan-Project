0700af02c1dba92f113693ceb7cd9a6bfc69f151
Tito-74 <kipkirui133@gmail.com>
2021-07-17 13:59:05 +0300

    added a formula

--- java.js
+++ java.js
@@ -5,7 +5,15 @@ function print(){
    var female = ["Akosua"," Adwoa","Abenaa"," Akua","Yaa","Afua"," Ama"];
     var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    document.getElementById("predict").innerHTML="hello"+ day;
+
+
+    var cc=parseInt(Birth.slice(0,2))
+    var yy=parseInt(Birth.slice(2,4))
+    var mm=parseInt(Birth.slice(5,7))
+    var dd=parseInt(Birth.slice(8,10))
+
+       var w_day = Math.abs((((cc/4)-2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)%7);
+    document.getElementById("predict").innerHTML="hello"+ w_day;
     
 
 }
