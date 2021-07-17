4b93d47da09836f765468fbf17449120a6386bba
Tito-74 <kipkirui133@gmail.com>
2021-07-17 13:51:44 +0300

    add a function to fatch data

---
+++ java.js
@@ -0,0 +1,11 @@
+function print(){
+    var Birth = document.getElementById("date").value;
+    var gender = document.getElementById("gender").value;
+   var male = [" Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+   var female = ["Akosua"," Adwoa","Abenaa"," Akua","Yaa","Afua"," Ama"];
+    var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    document.getElementById("predict").innerHTML="hello"+ day;
+    
+
+}
